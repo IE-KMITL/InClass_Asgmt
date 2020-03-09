187211441cf0fe32fbd2393dbc6c53095a3d7058
NATNAREE DUANGPIARACH <59952924+Pinniestark@users.noreply.github.com>
2020-03-09 11:36:49 +0700

    Add files via upload

---
+++ InClass-Asgmt #1/AnalogIn_LineDetection.ino
@@ -0,0 +1,57 @@
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = 36;  // Analog input pin that the potentiometer is attached to
+//const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  // change the analog out value:
+  //analogWrite(analogOutPin, outputValue);
+   if (sensorValue>100&&sensorValue<200){
+    
+      Serial.println("color is = YELLOW" );
+      
+   }
+    else if (sensorValue>500&&sensorValue<700){
+    
+      Serial.println("color is = GREEN");
+      
+    }
+    else if (sensorValue>1100&&sensorValue<1250){
+    
+      Serial.println("color is = BLUE");
+      
+    }
+    else {
+      Serial.println("color undefind");
+      
+    }
+     
+    
+
+  
+    
+    
+     
+
+  // print the results to the Serial Monitor:
+  /*Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+  */
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(250);
+}
